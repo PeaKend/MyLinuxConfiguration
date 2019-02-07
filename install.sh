44f9eef1de35313ca1e607cd2045085043365c1e
Facundo Atrio <peakend22@gmail.com>
2019-02-07 18:15:03 -0300

    added a update before retriving packages

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DISTRO_NAME=`./getDistro.py`
 # installs dependences according to the linux distro
 
 if [[ $DISTRO_NAME == "ubuntu"  ]]; then
-    sudo apt install inkscape make automake pkg-config vim tmux libgtk2.0-dev sassc -y
+    sudo apt update && sudo apt install inkscape make automake pkg-config vim tmux libgtk2.0-dev sassc -y
 fi
 
 # installs Adapta following their instructions at https://github.com/adapta-project/adapta-gtk-theme
